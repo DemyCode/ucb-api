57b94ef9250d65581424a56f4ee1ae4abe56d59d
DemyCode <verycols@gmail.com>
2023-02-02 22:13:42 +0100

    fix: FIxing config

--- app/config.py
+++ app/config.py
@@ -1,13 +1,27 @@
-from typing import List
+from typing import Any, Dict, List, Optional
 
-from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "ucb-api"
     API_V1_STR: str = "/api/v1"
 
-    DATABASE_URL: PostgresDsn = PostgresDsn("postgresql+asyncpg://postgres:postgres@db:5432/postgres")
+    DATABASE_URL: Optional[PostgresDsn]
+
+    @validator("DATABASE_URL", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     UPDATE_ALEMBIC: bool = False
